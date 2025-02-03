490b0340c760ea4fff6a099bc004be6f934751aa
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-02-03 01:54:02 +0000

    works as list of dicts

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,61 @@
+use common_macros::hash_map;
 use pyo3::prelude::*;
 use pyo3_ffi::c_str;
+use std::collections::HashMap;
 
 fn main() {
     pyo3::prepare_freethreaded_python();
 
-    let add_code = c_str!(
+    let code = c_str!(
         r#"
 import sys
 
 print (sys.version)
 	
-def test(d:str) -> str:
-    print (d)
+def test(d:list) -> list:
+    #print (d)
+	
+    for entry in d:
+        print ("e")
+	
+    d.append({"a":"b"})
     return d
 	
 "#
     );
 
+    let msgs = vec![
+        hash_map! {
+            "role"=>"system",
+            "content"=>"Help  to answer geographical questions."
+        },
+        hash_map! {
+            "role"=>"user",
+            "content"=> "What is the capital of Italy?",
+        },
+    ];
+
+    println!("{:?}", msgs);
+
     Python::with_gil(|py| {
         let module = PyModule::from_code(
-            //
-            py,
-            add_code,
+            py, //
+            code,
             c_str!(""),
             c_str!(""), //
         )
         .expect("Module code should be loaded");
 
-        let fn_test = module.getattr("test").expect("test should be there");
-        
-		
-		
-        let result_value: String = fn_test
-            .call1(("asdf",))
+        let fn_test = module //
+            .getattr("test")
+            .expect("test should be there");
+
+        let result_value: Vec<HashMap<String, String>> = fn_test
+            .call1((msgs,))
             .expect("result expected")
             .extract()
             .expect("value expected");
 
-        println!("{}", result_value)
+        println!("{:?}", result_value)
     });
 }
