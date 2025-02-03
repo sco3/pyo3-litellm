f330aa0cf65becb3ec5c1b132664416f675406af
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-02-03 01:00:23 +0000

    something works

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use pyo3::prelude::*;
+use pyo3_ffi::c_str;
+
+fn main() {
+    pyo3::prepare_freethreaded_python();
+
+    let add_code = c_str!(
+        r#"
+import sys
+
+print (sys.version)
+	
+def test(d:str) -> str:
+    print (d)
+    return d
+	
+"#
+    );
+
+    Python::with_gil(|py| {
+        let module = PyModule::from_code(
+            //
+            py,
+            add_code,
+            c_str!(""),
+            c_str!(""), //
+        )
+        .expect("Module code should be loaded");
+
+        let fn_test = module.getattr("test").expect("test should be there");
+        println!("{:?}", fn_test);
+        let result_value: String = fn_test
+            .call1(("asdf",))
+            .expect("result expected")
+            .extract()
+            .expect("value expected");
+
+        println!("{}", result_value)
+    });
+}
