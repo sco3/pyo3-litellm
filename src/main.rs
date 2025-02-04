6784ce00c11f66569d5d8600863fa06028f5db2c
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-02-04 00:18:50 +0000

    works with dict

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use common_macros::hash_map;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
+use pyo3::types::PyInt;
+use pyo3::types::PyString;
+use pyo3::types::PyTuple;
 use pyo3_ffi::c_str;
+use std::time::Instant;
 
 fn main() {
     pyo3::prepare_freethreaded_python();
@@ -46,11 +50,31 @@ def test(d:list) -> list:
 
         let r = fn_test //
             .call1((msgs,));
-
+        let start = Instant::now();
         if let Ok(r) = r {
             if let Ok(dict) = r.downcast::<PyDict>() {
-                println!("dict! {:?}", dict);
+                println!("dict! {}", dict);
+                println!("items: {}", dict.items());
+                for item in dict.items() {
+                    println!("item: {}", item.get_type());
+                    if let Ok(tuple) = item.downcast::<PyTuple>() {
+                        println!("tuple: {}", tuple);
+                        for idx in 0..tuple.len() {
+                            let v = tuple.get_item(idx);
+                            println!("value{}: {:?}", idx, v);
+                            if let Ok(v) = v {
+                                if let Ok(s) = v.downcast::<PyString>() {
+                                    println!("str{}: {}", idx, s);
+                                }
+                                if let Ok(i) = v.downcast::<PyInt>() {
+                                    println!("str{}: {}", idx, i);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+        println!("Time: {} mks", start.elapsed().as_micros());
     });
 }
